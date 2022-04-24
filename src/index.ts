9e391801efc5b96e4308d5dca2b092a831f76d69
Esau Orozco <esau.orozco.r@gmail.com>
2022-04-24 13:26:37 -0500

    Add morgan

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import express, { Express } from 'express';
+import morgan from 'morgan';
 
 import weatherRouter from './routes/weather.route';
 
 const app: Express = express();
 const port: string | number = process.env.PORT || 3001;
+
+// Middlewares
+app.use(express.json());
+app.use(morgan('tiny'));
 
 app.use('/api/weather', weatherRouter);
 

ccd8adac44431166508dcfb1c262c386f461b125
Esau Orozco <esau.orozco.r@gmail.com>
2022-04-25 12:27:46 -0500

    add weather validation schema

--- src/routes/weather.route.ts
+++ src/routes/weather.route.ts
@@ -5,10 +5,15 @@ import { WeatherController } from '../controllers/weather.controller';
 import validate from '../utils/schemeValidator.util';
 
 const locationSchema = [query('name', 'Name is required').exists()];
+const weatherSchema = [
+  query('lon', 'Longitude is required').exists().isNumeric(),
+  query('lat', 'Latitude is required').exists().isNumeric(),
+  query('units', 'Units value can only be metric or imperial').optional().isIn(['metric', 'imperial']),
+];
 
 const router = express.Router();
 
 router.get('/location', validate(locationSchema), WeatherController.getLocation);
-router.get('/weather', WeatherController.getWeather);
+router.get('/weather', validate(weatherSchema), WeatherController.getWeather);
 
 export default router;

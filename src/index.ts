e5b8e9f797d66370efb0a51c70c51338f16963d9
Esau Orozco <esau.orozco.r@gmail.com>
2022-05-02 09:54:51 -0500

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import express, { Express } from 'express';
 import morgan from 'morgan';
 
@@ -5,11 +6,20 @@ import weatherRouter from './routes/weather.route';
 
 const app: Express = express();
 const port: string | number = process.env.PORT || 3001;
+const allowedOrigins = process.env.APP_URI?.split(',').map((url: string) => `${url}`);
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
 
 // Middlewares
 app.use(express.json());
 app.use(morgan('tiny'));
+app.use(cors(options));
 
 app.use('/api', weatherRouter);
+
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Allowed origin: ', allowedOrigins);
+}
 
 app.listen(port, () => console.log(`LWeather API is running on port ${port}`));
